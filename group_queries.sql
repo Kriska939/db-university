45af74ade8163980b0d2b83620915128b4e8b398
Kriska939 <cristinacorrado93@gmail.com>
2021-09-28 15:52:27 +0200

    1 EX-GROUP done

---
+++ group_queries.sql
@@ -0,0 +1,7 @@
+/* ESERCIZI QUERY CON: GROUP BY*/
+
+-- 1. Contare quanti iscritti ci sono stati ogni anno
+
+SELECT COUNT(id), YEAR(`enrolment_date`)
+FROM `students`
+GROUP BY YEAR(`enrolment_date`);
