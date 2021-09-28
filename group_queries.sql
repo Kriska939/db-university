5388f9658cfef192318de8b92b164aa82e2b89d4
Kriska939 <cristinacorrado93@gmail.com>
2021-09-28 16:07:00 +0200

    completed all exercises

--- group_queries.sql
+++ group_queries.sql
@@ -5,3 +5,20 @@
 SELECT COUNT(id), YEAR(`enrolment_date`)
 FROM `students`
 GROUP BY YEAR(`enrolment_date`);
+
+-- 2. Contare gli insegnanti che hanno l'ufficio nello stesso edificio
+
+SELECT COUNT(id), `office_address`
+FROM `teachers`
+GROUP BY `office_address`;
+
+-- 3. Calcolare la media dei voti di ogni appello d'esame
+
+SELECT `exam_id`, ROUND(AVG(`vote`)) AS `average_vote` FROM `exam_student`
+GROUP BY `exam_id`;
+
+-- 4. Contare quanti corsi di laurea ci sono per ogni dipartimento
+
+SELECT COUNT(id) AS `n_courses`, `department_id` 
+FROM `degrees`
+GROUP BY `department_id`;
