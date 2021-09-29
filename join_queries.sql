898884adf6c859b31decc2c9f9a686e2e8f54c72
Kriska939 <cristinacorrado93@gmail.com>
2021-09-29 16:07:07 +0200

    EX-JOIN 4 complete

--- join_queries.sql
+++ join_queries.sql
@@ -25,7 +25,12 @@ WHERE `teachers`.`id`= 44;
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
-
+SELECT `students`. `name`, `students`. `surname`, `degrees`. `name`, `departments`. `name`
+FROM `students`
+INNER JOIN `degrees` ON `students`.`degree_id` = `degrees`.`id`
+INNER JOIN `departments` ON `degrees`.`department_id` = `departments`.`id`
+/* NB: ordine alfabetico per cognome E nome: */
+ORDER BY `students`. `surname`,`students`. `name`
 
 -- 5. Selezionare tutti i corsi di laurea con i relativi corsi e insegnanti
 
