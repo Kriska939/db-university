f169bd5f3c32c516071abf6a43278b4187e8eac6
Kriska939 <cristinacorrado93@gmail.com>
2021-09-30 09:37:03 +0200

    small fix

--- join_queries.sql
+++ join_queries.sql
@@ -21,7 +21,7 @@ SELECT `teachers`. `name`, `teachers`. `surname`, `courses`.`name`
 FROM `courses`
 JOIN `course_teacher` ON `courses`.`id` = `course_teacher`.`course_id`
 JOIN `teachers` ON `course_teacher`.`teacher_id` = `teachers`.`id`
-WHERE `teachers`.`id`= 44;
+WHERE `teachers`.`name`= "Fulvio" AND `teachers`.`surname`= "Amato";
 
 -- 4. Selezionare tutti gli studenti con i dati relativi al corso di laurea a cui sono iscritti e il relativo dipartimento, in ordine alfabetico per cognome e nome
 
